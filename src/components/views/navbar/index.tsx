40b9a4a7ab7e4fad595825d901dc53cddaa9c911
MuradAles <muradaleskerov99@gmail.com>
2024-03-12 15:55:55 -0400

    logo been added

--- src/components/views/navbar/index.tsx
+++ src/components/views/navbar/index.tsx
@@ -1,12 +1,13 @@
 import "./_navbar.scss";
-
+import { BulbLogo } from "../../../assets/bulb-logo";
 const NavBar = () => {
-    return (
-        <div className="NavBar">
-            <p>Home</p>
-            <p>Test</p>
-        </div>
-    );
-  };
+  return (
+    <div className="NavBar">
+      <BulbLogo width={"90px"} height={"90px"} />
+      <p>Home</p>
+      <p>Test</p>
+    </div>
+  );
+};
 
 export default NavBar;
